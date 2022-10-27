dcce9bcab5f5bc9295a64275f91eb62c6502dc28
rakshith <rakshithjogihalli@gmail.com>
2022-10-27 14:02:43 +0530

    added bean return controller

--- src/main/java/org/helloworld/controllers/HelloWorld.java
+++ src/main/java/org/helloworld/controllers/HelloWorld.java
@@ -1,18 +1,26 @@
 package org.helloworld.controllers;
 
+import org.helloworld.models.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class HelloWorld {
-    @GetMapping
-    public String sayHelloWorld(HttpServletResponse response){
+    @GetMapping("/say")
+    public String sayHelloWorld(HttpServletResponse response) {
         response.setStatus(HttpStatus.OK.value());
         return "hello world..!";
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers(HttpServletResponse response) {
+        response.setStatus(HttpStatus.OK.value());
+        return List.of(new User("Rakshith", "R", "Jogihalli"));
     }
 }
---
+++ src/main/java/org/helloworld/models/User.java
@@ -0,0 +1,16 @@
+package org.helloworld.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String firstName;
+    private String lastName;
+    private String city;
+}
